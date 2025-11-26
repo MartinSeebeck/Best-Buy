ada55a1b5423ed45bf35e8c617f1b295fe6bb9c0
Balthasar von Weymarn <grainofsalt@gmx.net>
2025-11-26 15:24:06 +0100

    Typo in main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """
 Main module to init store CLI.
 """
+
 import products
 from store import Store
 
@@ -13,27 +14,26 @@ def main():
         products.Product("Auchentoshan Malts of Scotland 1998 Rare Casks", price=349, quantity=30),
     ]
 
-        # Est a store instance with a product list
-        best_buy = Store(product_list)
+    # Est a store instance with a product list
+    best_buy = Store(product_list)
 
-        # Test the method of Store class
-        print(f"Gesamtmenge im Store: {best_buy.get_total_quantity()}")
+    # Test the method of Store class
+    print(f"Gesamtmenge im Store: {best_buy.get_total_quantity()}")
 
-        # Get all active products
-        active_products = best_buy.get_all_products()
+    # Get all active products
+    active_products = best_buy.get_all_products()
 
-        # Build and process order
-        # E.g. Ordering 1 Lagavulin (idx 0) und 2x Ardbeg (idx 1)
-        shopping_list = [(active_products[0], 1), (active_products[1], 2)]
+    # Build and process order
+    # E.g. Ordering 1 Lagavulin (idx 0) und 2x Ardbeg (idx 1)
+    shopping_list = [(active_products[0], 1), (active_products[1], 2)]
 
-        order_total = best_buy.order(shopping_list)
-        print(f"Total amount due: {order_total}")
+    order_total = best_buy.order(shopping_list)
+    print(f"Total amount due: {order_total}")
 
-        # Show the actualized qty after order
-        print("\nStore inventory after this order:")
-        for product in best_buy.get_all_products():
-            print(product.show())
-
+    # Show the actualized qty after order
+    print("\nStore inventory after this order:")
+    for product in best_buy.get_all_products():
+        print(product.show())
         print(f"\nTotal inventory of all products after order: {best_buy.get_total_quantity()}")
 
 
